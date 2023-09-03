03dfb81acff57d279f98870afffb375e334752e7
Marco <cwhong8087@gmail.com>
2023-09-03 16:33:20 +0900

    13th commit

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,17 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // import {
 //     useDispatch,
 //     useSelector,
 // } from '../../../node_modules/react-redux/es/exports';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { useNavigate } from '../../../../../../node_modules/react-router-dom/dist/index'; // 원본
+// import { useNavigate } from 'react-router-dom';
 
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.login,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user,
     }));
+    const navigate = useNavigate(); // 책에는 이게 빠져있음(트러블 슈팅)
+
     // 인풋 변경 이벤트 핸들러
     const onChange = (e) => {
         const { value, name } = e.target;
@@ -27,7 +36,8 @@ const LoginForm = () => {
     // 폼 등록 이벤트 핸들러
     const onSubmit = (e) => {
         e.preventDefault();
-        // 추후 구현
+        const { username, password } = form;
+        dispatch(login({ username, password }));
     };
 
     // 컴포넌트가 처음 렌더링될 시 form을 초기화함
@@ -35,12 +45,32 @@ const LoginForm = () => {
         dispatch(initializeForm('login'));
     }, [dispatch]);
 
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            setError('로그인 실패');
+            return;
+        }
+        if (auth) {
+            console.log('로그인 성공');
+            dispatch(check());
+        }
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+        if (user) {
+            navigate('/');
+        }
+    }, [navigate, user]);
+
     return (
         <AuthForm
             type="login"
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // import {
 //     useDispatch,
 //     useSelector,
@@ -7,8 +7,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
+import { useNavigate } from '../../../../../../node_modules/react-router-dom/dist/index';
 
 const RegisterForm = () => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.register,
@@ -16,6 +18,8 @@ const RegisterForm = () => {
         authError: auth.authError,
         user: user.user,
     }));
+    const navigate = useNavigate();
+
     // 인풋 변경 이벤트 핸들러
     const onChange = (e) => {
         const { value, name } = e.target;
@@ -32,8 +36,24 @@ const RegisterForm = () => {
     const onSubmit = (e) => {
         e.preventDefault();
         const { username, password, passwordConfirm } = form;
+        // 하나라도 비어 있다면
+        if ([username, password, passwordConfirm].includes('')) {
+            setError('빈 칸을 모두 입력하세요.');
+            return;
+        }
+        // 비번 일치 여부
         if (password !== passwordConfirm) {
-            // TODO: 오류처리
+            setError('비밀번호가 일치하지 않습니다.');
+            dispatch(
+                changeField({ form: 'register', key: 'password', value: '' }),
+            );
+            dispatch(
+                changeField({
+                    form: 'register',
+                    key: 'passwordConfirm',
+                    value: '',
+                }),
+            );
             return;
         }
         dispatch(register({ username, password }));
@@ -47,8 +67,13 @@ const RegisterForm = () => {
     // 회원가입 성공/실패 처리
     useEffect(() => {
         if (authError) {
-            console.log('오류 발생!');
-            console.log(authError);
+            // 계정명이 이미 존재할 시
+            if (authError.response.status === 409) {
+                setError('이미 존재하는 계정입니다.');
+                return;
+            }
+            // 기타 이유
+            setError('회원가입 실패');
             return;
         }
         if (auth) {
@@ -61,10 +86,11 @@ const RegisterForm = () => {
     // user 값이 잘 설정되었는지 확인
     useEffect(() => {
         if (user) {
-            console.log('check API 성공!');
-            console.log(user);
+            if (user) {
+                navigate('/');
+            }
         }
-    }, [user]);
+    }, [navigate, user]);
 
     return (
         <AuthForm
@@ -72,6 +98,7 @@ const RegisterForm = () => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
